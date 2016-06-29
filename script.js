da3832395f948319440d8783947a550e634356e9
Jerry Lee <jerry.lee09@yahoo.com>
2016-06-28 22:34:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function add(value) {
+	return value += 2;
+}
+
+function multiply(value) {
+	return value *= 3;
+}
+
+function subtract(value) {
+	return value -= 4;
+}
+
+add(multiply(subtract(2)));
